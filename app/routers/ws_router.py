a4ec552d2f67e389714c4914131ad708d0f68e0d
Shravan Parikh <parikhshravan@gmail.com>
2025-09-26 13:37:14 +0530

    flake8 error fixed

--- app/routers/ws_router.py
+++ app/routers/ws_router.py
@@ -75,7 +75,7 @@ class OutputStreamRouter:
         self._streamers.pop(cam, None)
         self._frames.pop(cam, None)
         logger.info(f"Camera {cam} completely removed")
-    
+
     def remove_publisher(self, cam: str) -> None:
         """Remove publisher but keep camera and subscribers if they exist"""
         if cam in self._frames:
@@ -83,7 +83,7 @@ class OutputStreamRouter:
             logger.info(f"Publisher frames cleared for camera {cam}, but keeping subscribers")
         else:
             logger.info(f"No frames to clear for camera {cam}")
-    
+
     def has_subscribers(self, cam: str) -> bool:
         """Check if camera has any subscribers"""
         return cam in self._streamers and len(self._streamers[cam]) > 0
@@ -127,7 +127,7 @@ class OutputStreamRouter:
             if sid in self._streamers[cam]:
                 self._streamers[cam].pop(sid)
                 logger.info(f"Subscriber {sid} removed from camera {cam}")
-                
+
                 # If no subscribers left and no keep-alive, remove camera completely
                 if len(self._streamers[cam]) == 0:
                     logger.info(f"No subscribers left for camera {cam}")
