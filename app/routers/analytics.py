0c82c46fc2744d0e6fd63a9c12f3838dd5c84907
suvindran3 <suvindran2001@gmail.com>
2025-12-16 19:35:46 +0530

    add raw output endpoint

--- app/routers/analytics.py
+++ app/routers/analytics.py
@@ -12,8 +12,16 @@ router = APIRouter(prefix="/v1/analytics", tags=["Analytics"])
 
 
 @router.get("/cumulative-count")
-def get_cumulative_count(start_time: int, end_time: int, db: Session = Depends(get_db)):
-    query = SQLFactory.get_query(SQLQuery.CUMULATIVE_COUNT, start_time=start_time, end_time=end_time)
+def get_cumulative_count(start_time: int, end_time: int, output_key: str, db: Session = Depends(get_db)):
+    query = SQLFactory.get_query(SQLQuery.CUMULATIVE_COUNT, start_time=start_time, end_time=end_time, output_key=output_key)
+    result = db.execute(text(textwrap.shorten(query, 1e8)))
+    decoded = [gen_util.format_nested_dict(dict(r)) for r in result.mappings().all()]
+    return gen_util.form_generic_response(decoded)
+
+
+@router.get("/raw-output")
+def get_cumulative_count(start_time: int, end_time: int, pipeline_id: int, db: Session = Depends(get_db)):
+    query = SQLFactory.get_query(SQLQuery.RAW_OUTPUT, start_time=start_time, end_time=end_time, pipeline_id=pipeline_id)
     result = db.execute(text(textwrap.shorten(query, 1e8)))
     decoded = [gen_util.format_nested_dict(dict(r)) for r in result.mappings().all()]
     return gen_util.form_generic_response(decoded)
--- app/utils/sql_factory.py
+++ app/utils/sql_factory.py
@@ -3,18 +3,37 @@ import enum
 
 class SQLQuery(enum.Enum):
     CUMULATIVE_COUNT = "cumulative_count"
+    RAW_OUTPUT = "raw_output"
 
 
 class SQLFactory:
     @staticmethod
     def get_query(q: SQLQuery, **kwargs):
         if q == SQLQuery.CUMULATIVE_COUNT:
-            return f""""
+            return f"""
             SELECT 
                 output_value,
-                COUNT(output_value) AS count,
+                COUNT(output_value) AS count
             FROM
                 pipeline_session_output_unit
             WHERE
-                created_at >= {kwargs.get("start_time")} AND created_at <= {kwargs.get("end_time")} AND output_key = {kwargs.get("output_key")};
+                created_at >= {kwargs.get("start_time")} AND created_at <= {kwargs.get("end_time")} AND output_key = '{kwargs.get("output_key")}'
+            GROUP BY 
+                output_value;
             """
+
+        elif q == SQLQuery.RAW_OUTPUT:
+            return f"""
+            SELECT 
+                psou.*
+            FROM
+                pipeline_session_output_unit psou
+            JOIN
+                pipeline_session_output pso ON pso.id = psou.pipeline_session_output_id
+            JOIN
+                pipeline_session ps ON ps.id = pso.pipeline_session_id AND ps.pipeline_id = {kwargs.get("pipeline_id")}
+            WHERE
+                psou.created_at >= {kwargs.get("start_time")} AND psou.created_at <= {kwargs.get("end_time")};
+            """
+
+        return None
