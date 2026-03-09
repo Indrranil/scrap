8b3a6c39d1ed8a532e95acb4bc57e89526380e8d
suvindran3 <suvindran2001@gmail.com>
2026-03-09 16:51:56 +0530

    add cumulative value endpoint

--- app/routers/analytics.py
+++ app/routers/analytics.py
@@ -20,8 +20,26 @@ def get_cumulative_count(start_time: int, end_time: int, output_key: str, db: Se
 
 
 @router.get("/raw-output")
-def get_cumulative_count(start_time: int, end_time: int, pipeline_id: int, db: Session = Depends(get_db)):
+def get_raw_output_by_pipeline(start_time: int, end_time: int, pipeline_id: int, db: Session = Depends(get_db)):
     query = SQLFactory.get_query(SQLQuery.RAW_OUTPUT, start_time=start_time, end_time=end_time, pipeline_id=pipeline_id)
+    result = db.execute(text(textwrap.shorten(query, 1e8)))
+    decoded = [gen_util.format_nested_dict(dict(r)) for r in result.mappings().all()]
+    return gen_util.form_generic_response(decoded)
+
+
+@router.get("/output-by-value")
+def get_output_by_value(output_key: str, output_value: str, page_no: int = 1, db: Session = Depends(get_db)):
+    limit = 50
+    offset = (page_no - 1) * limit
+    query = SQLFactory.get_query(SQLQuery.OUTPUT_BY_VALUE, output_key=output_key, output_value=output_value, limit=limit, offset=offset)
+    result = db.execute(text(textwrap.shorten(query, 1e8)))
+    decoded = [gen_util.format_nested_dict(dict(r)) for r in result.mappings().all()]
+    return gen_util.form_generic_response(decoded)
+
+
+@router.get("/cumulative-value")
+def get_output_by_value(output_key: str, suffix: str, start_time: int, end_time: int, pipeline_id: int, db: Session = Depends(get_db)):
+    query = SQLFactory.get_query(SQLQuery.CUMULATIVE_VALUE, output_key=output_key, suffix=suffix, start_time=start_time, end_time=end_time, pipeline_id=pipeline_id)
     result = db.execute(text(textwrap.shorten(query, 1e8)))
     decoded = [gen_util.format_nested_dict(dict(r)) for r in result.mappings().all()]
     return gen_util.form_generic_response(decoded)
--- app/utils/sql_factory.py
+++ app/utils/sql_factory.py
@@ -4,6 +4,8 @@ import enum
 class SQLQuery(enum.Enum):
     CUMULATIVE_COUNT = "cumulative_count"
     RAW_OUTPUT = "raw_output"
+    OUTPUT_BY_VALUE = "output_by_value"
+    CUMULATIVE_VALUE = "cumulative_value"
 
 
 class SQLFactory:
@@ -36,4 +38,58 @@ class SQLFactory:
                 psou.created_at >= {kwargs.get("start_time")} AND psou.created_at <= {kwargs.get("end_time")};
             """
 
+        elif q == SQLQuery.OUTPUT_BY_VALUE:
+            return f"""
+            WITH results AS (
+                SELECT DISTINCT(pso.id) AS id
+                FROM pipeline_session ps
+                JOIN pipeline_session_output pso ON ps.id = pso.pipeline_session_id
+                JOIN pipeline_session_output_unit psou ON pso.id = psou.pipeline_session_output_id and psou.output_key = "{kwargs.get("output_key")}" and psou.output_value = "{kwargs.get("output_value")}"
+            )
+            SELECT 
+                psou.*
+            FROM
+                pipeline_session_output_unit psou
+            JOIN
+                results r
+            JOIN
+                pipeline_session_output pso ON pso.id = psou.pipeline_session_output_id AND r.id =pso.id
+            JOIN
+                pipeline_session ps ON ps.id = pso.pipeline_session_id
+            ORDER BY
+                psou.id DESC
+            LIMIT
+                {kwargs.get('limit')}
+            OFFSET
+                {kwargs.get('offset')};
+            """
+
+        elif q == SQLQuery.CUMULATIVE_VALUE:
+            return f"""
+            SELECT 
+                SUM(CAST(REPLACE(psou.output_value, '{kwargs.get("suffix")}', '') AS DECIMAL(10,3))) AS cumulative_value
+            FROM
+                pipeline_session_output_unit psou
+            JOIN
+                pipeline_session_output pso ON pso.id = psou.pipeline_session_output_id
+            JOIN
+                pipeline_session ps ON ps.id = pso.pipeline_session_id AND ps.pipeline_id = {kwargs.get("pipeline_id")}
+            WHERE
+                psou.created_at >= {kwargs.get("start_time")} AND psou.created_at <= {kwargs.get("end_time")} AND psou.output_key = '{kwargs.get("output_key")}';
+            """
+
         return None
+
+
+f"""
+SELECT 
+                SUM(CAST(REPLACE(psou.output_value, 'kg', '') AS DECIMAL(10,3))) AS cumulative_value
+            FROM
+                pipeline_session_output_unit psou
+            JOIN
+                pipeline_session_output pso ON pso.id = psou.pipeline_session_output_id
+            JOIN
+                pipeline_session ps ON ps.id = pso.pipeline_session_id AND ps.pipeline_id = 4
+            WHERE
+                psou.created_at >= 0 AND psou.created_at <= 1773054714 AND psou.output_key = 'weight';
+"""
